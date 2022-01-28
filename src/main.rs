093e56684738d690377c2150e261bcbba6bb5318
Blackfur <64478051+TheBlackfurGuy@users.noreply.github.com>
2022-01-28 17:45:22 +0100

    welp

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,40 @@
 use yew::prelude::*;
 
-enum Msg {
-    AddOne,
+pub enum Msg {
+    Stormy,
+    Sunny,
 }
 
-struct Model {
-    value: i64,
+pub struct App {
+    weather: bool,
 }
 
-impl Component for Model {
+#[allow(unused_variables)]
+impl Component for App {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_ctx: &Context<Self>) -> Self {
+    fn create(ctx: &Context<Self>) -> Self {
         Self {
-            value: 0,
+            weather: false,
         }
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
+            Msg::Stormy => self.weather = true,
+            Msg::Sunny => self.weather = false,
         }
+        true
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
-        let link = ctx.link();
-        html! {
-            <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
-        }
+        html!(
+            <p>{ self.weather}</p>
+        )
     }
 }
 
-fn main() {
-    yew::start_app::<Model>();
+fn main(){
+    yew::start_app::<App>();
 }
