f921f4522b96b9051fb955f0363ad09c5579dc52
Blackfur <64478051+theblackfurguy@users.noreply.github.com>
2022-01-29 16:33:58 +0100

    thingys

--- dist/index-6cdbe6b5e9a3d62a.js
+++ dist/index-6cdbe6b5e9a3d62a.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-e63830e1de63004_bg.wasm', import.meta.url);
+        input = new URL('index-6cdbe6b5e9a3d62a_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -474,8 +474,8 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper196 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 50, __wbg_adapter_18);
+    imports.wbg.__wbindgen_closure_wrapper198 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 51, __wbg_adapter_18);
         return addHeapObject(ret);
     };
 
