26ed00d3aed9b9028ad8c5920974f1eb06982ec8
Blackfur <64478051+theblackfurguy@users.noreply.github.com>
2022-01-29 12:07:59 +0100

    merge or something idk

--- dist/index-e63830e1de63004.js
+++ dist/index-e63830e1de63004.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-779fb93a35d4453b_bg.wasm', import.meta.url);
+        input = new URL('index-e63830e1de63004_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
