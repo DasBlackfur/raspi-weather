64e0afe1f7ab956cdc137fbee9623529705f7d7e
Blackfur <64478051+TheBlackfurGuy@users.noreply.github.com>
2022-01-31 14:47:01 +0100

    test button thingy

--- src/main.rs
+++ src/main.rs
@@ -54,6 +54,9 @@ impl Component for App {
                             <button class="button" onclick={ctx.link().callback(|_| Msg::Settings)}>
                                 { "Exit Settings" }
                             </button>
+                            <a href="test.html">
+                                { "Go to test.html" }
+                            </a>
                         </div>
                         <style>
                             { "body {" }
