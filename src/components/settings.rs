444e681101adac64ad2293402cd2a2964aef267f
Blackfur <64478051+TheBlackfurGuy@users.noreply.github.com>
2022-02-14 14:30:47 +0100

    maybe thsi wrosk

--- src/components/settings.rs
+++ src/components/settings.rs
@@ -12,9 +12,15 @@ impl Component for SettingsComponent {
     }
 
     fn view(&self, ctx: &yew::Context<Self>) -> yew::Html {
+        let options = js_sys::Object::new();
+        js_sys::Reflect::set(&options, &"weekday".into(), &"short".into()).unwrap();
+        js_sys::Reflect::set(&options, &"year".into(), &"numeric".into()).unwrap();
+        js_sys::Reflect::set(&options, &"month".into(), &"short".into()).unwrap();
+        js_sys::Reflect::set(&options, &"day".into(), &"numeric".into()).unwrap();
         html!{
             <p style="font-size: 70%">
-                { js_sys::Date::new_0().to_locale_time_string("de-DE").to_string() }
+                { js_sys::Date::new_0().to_locale_time_string("de-DE").to_string() } <br/>
+                { js_sys::Date::new_0().to_locale_date_string("de-DE", &options).to_string() }
             </p>
         }
     }
