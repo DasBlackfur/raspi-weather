4d2ce336eaeb9bd4d5bec24adedd102e57b9b58c
Blackfur <64478051+TheBlackfurGuy@users.noreply.github.com>
2022-04-02 19:04:06 +0200

    YE YE HAW

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,6 +1,5 @@
 pub mod settings;
 pub mod temperature;
-pub mod weather;
 pub mod wind_angle;
 pub mod co2;
 pub mod humidity;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use gloo::timers::callback::Interval;
+use reqwasm::http::Request;
 use yew::prelude::*;
 
 mod components;
 
 use components::temperature::TemperatureComponent;
-use components::weather::WeatherComponent;
 use components::wind_angle::WindAngleComponent;
 use components::settings::SettingsComponent;
 use components::co2::CO2Component;
@@ -14,6 +15,7 @@ pub enum Msg {
     Update,
     Settings,
     Increment,
+    Value(String),
 }
 
 pub struct App {
@@ -24,6 +26,7 @@ pub struct App {
     co2: u16,
     humidity: u8,
     wind_speed: i16,
+    interval: Interval
 }
 
 #[allow(unused_variables)]
@@ -32,6 +35,10 @@ impl Component for App {
     type Properties = ();
 
     fn create(ctx: &Context<Self>) -> Self {
+        let data_handle = {
+            let link = ctx.link().clone();
+            Interval::new(1000, move || link.send_message(Msg::Update))
+        };
         Self {
             weather: false,
             settings: false,
@@ -40,14 +47,27 @@ impl Component for App {
             co2: 800,
             humidity: 50,
             wind_speed: 0,
+            interval: data_handle
         }
     }
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
             Msg::Update => {
-                self.weather = !self.weather;
-                true
+                ctx.link().send_future(async {
+                    let response = Request::get("https://api.netatmo.com/api/getstationsdata?get_favorites=false")
+                        .header("Authorization", "Bearer TOKEN")
+                        .header("accept", "application/json")
+                        .send()
+                        .await
+                        .unwrap()
+                        .text()
+                        .await
+                        .unwrap();
+                    
+                    Msg::Value(response)
+                });
+                false
             }
             Msg::Settings => {
                 self.settings = !self.settings;
@@ -61,6 +81,19 @@ impl Component for App {
                 self.wind_speed += 10;
                 true
             }
+            Msg::Value(val) => {
+                let thingy: serde_json::Value = serde_json::from_str(&val).unwrap();
+                self.temperature = thingy.pointer("/body/devices/0/dashboard_data/Temperature").unwrap().as_f64().unwrap() as f32;
+                self.humidity = thingy.pointer("/body/devices/0/dashboard_data/Humidity").unwrap().as_u64().unwrap() as u8;
+                self.co2 = thingy.pointer("/body/devices/0/dashboard_data/CO2").unwrap().as_u64().unwrap() as u16;
+                self.wind_angle = thingy.pointer("/body/devices/0/modules/2/dashboard_data/WindAngle").unwrap().as_i64().unwrap() as i16;
+                self.wind_speed = thingy.pointer("/body/devices/0/modules/2/dashboard_data/WindStrength").unwrap().as_i64().unwrap() as i16;
+                self.weather = match thingy.pointer("/body/devices/0/modules/1/dashboard_data/Rain").unwrap().as_i64().unwrap() {
+                    0 => false,
+                    _ => true,
+                };
+                true
+            },
         }
     }
 
@@ -71,7 +104,7 @@ impl Component for App {
                     <>
                         <div>
                             <button class="button" onclick={ctx.link().callback(|_| Msg::Update)}>
-                                { "Toggle Weather" }
+                                { "Update Data" }
                             </button>
 
                             <button class="button" onClick="window.location.reload();">
