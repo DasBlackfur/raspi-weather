58c833d6c094588b41ca21aa6c387a1b6e21cde8
blackfur <64478051+TheBlackfurGuy@users.noreply.github.com>
2022-10-03 12:32:32 +0200

    Made the time much larger

--- src/components/settings.rs
+++ src/components/settings.rs
@@ -43,9 +43,9 @@ impl Component for SettingsComponent {
         js_sys::Reflect::set(&options, &"day".into(), &"numeric".into()).unwrap();
         html! {
             <>
-                <p style="font-size: 60%; padding-left: 10px;">
+                <p style="font-size: 80%; padding-left: 10px;">
                     { js_sys::Date::new_0().to_locale_time_string("de-DE").to_string() } <br/>
-                    { js_sys::Date::new_0().to_locale_date_string("de-DE", &options).to_string() }
+                    // { js_sys::Date::new_0().to_locale_date_string("de-DE", &options).to_string() }
                 </p>
                 <button class="button" onclick={ctx.props().settings_callback.clone()}>
                     {" "}
