3926d74eea690b0f94e90accbedcf28da1174fe5
blackfur <64478051+TheBlackfurGuy@users.noreply.github.com>
2022-10-14 16:33:27 +0200

    fix prev commit

--- src/components/settings.rs
+++ src/components/settings.rs
@@ -47,7 +47,7 @@ impl Component for SettingsComponent {
                     { js_sys::Date::new_0().to_locale_time_string("de-DE").to_string() } <br/>
                     // { js_sys::Date::new_0().to_locale_date_string("de-DE", &options).to_string() }
                 </p>
-                <button class="button" onClick="window.location.reload();"}>
+                <button class="button" onClick="window.location.reload();">
                     {"⟳ "
                 </button>
             </>
