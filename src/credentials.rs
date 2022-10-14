7371ab946273406db73a1246c63e4f64627ce072
Blackfur <64478051+theblackfurguy@users.noreply.github.com>
2022-10-14 15:00:02 +0200

    please dont

--- src/credentials.rs
+++ src/credentials.rs
@@ -1,4 +1,4 @@
-pub static CLIENT_ID: &str = "";
-pub static CLIENT_SECRET: &str = "";
-pub static USERNAME: &str = "";
-pub static PASSWORD: &str = "";
+pub static CLIENT_ID: &str = "625be70c988522184430bfd2";
+pub static CLIENT_SECRET: &str = "940xjgcPGaOhJ8MWDPcfhvFlcoo4VJuycrfTA";
+pub static USERNAME: &str = "schwarzwald-12@gmx.net";
+pub static PASSWORD: &str = "4u4pd2I27G-A0I8zJ40CCHG92jn2HkQ";
