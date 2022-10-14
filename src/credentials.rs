52fa3cfcc8d12d2e626744b8173cd32077a900c2
Blackfur <64478051+theblackfurguy@users.noreply.github.com>
2022-10-14 15:01:34 +0200

    awa

--- src/credentials.rs
+++
@@ -1,4 +0,0 @@
-pub static CLIENT_ID: &str = "625be70c988522184430bfd2";
-pub static CLIENT_SECRET: &str = "940xjgcPGaOhJ8MWDPcfhvFlcoo4VJuycrfTA";
-pub static USERNAME: &str = "schwarzwald-12@gmx.net";
-pub static PASSWORD: &str = "4u4pd2I27G-A0I8zJ40CCHG92jn2HkQ";
